f4b15f3fea90b6819ef483c871de774cd134fcd7
unknown <amoljadhav>
2023-04-08 10:47:53 +0530

    [Amol]Add:WelcomeToTheAddressBookIO

---
+++ AddressBookIO/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace AddressBookIO
+{
+    public class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string ZIP;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
---
+++ AddressBookIO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the AddressBook IO System!");
+        }
+    }
+}
